252b8cedc5d9b03741faced1b6e568983e57a36a
Jerren Trifan <nerrej01@gmail.com>
2017-06-29 10:52:46 -0400

    Updated checks, changed original script for editing, changed read-only access, as well as changed VSync setting in GameUserSettings.ini.

--- DeadByDaylight144hz/Form1.cs
+++ DeadByDaylight144hz/Form1.cs
@@ -15,6 +15,7 @@ namespace DeadByDaylight144hz
     {
         static string userName = System.Environment.UserName;
         public string pathValue = @"C:\Users\" + userName + @"\AppData\Local\DeadByDaylight\Saved\Config\WindowsNoEditor\Engine.ini";
+        public string gamePathValue = null;
         Boolean pathSet = false;
 
         public Form1()
@@ -23,7 +24,11 @@ namespace DeadByDaylight144hz
             if (File.Exists(pathValue))
             {
                 pathSet = true;
+                gamePathValue = pathValue.Substring(0, pathValue.Length - 10) + "GameUserSettings.ini";
                 pathDisplayBox.Text = "" + pathValue + " (Auto-detected)";
+
+                File.SetAttributes(pathValue, File.GetAttributes(pathValue) & ~FileAttributes.ReadOnly); // Get Attributes, set read only off on Engine.ini
+                File.SetAttributes(gamePathValue, File.GetAttributes(gamePathValue) & ~FileAttributes.ReadOnly); // Get Attributes, set read only off on GameUserSettings.ini
             }
             else
                 pathDisplayBox.Text = "No path set";
@@ -39,11 +44,23 @@ namespace DeadByDaylight144hz
             if (pathValue.Contains("Engine.ini"))
             {
                 pathValue = openFileDialog1.FileName;
+                gamePathValue = pathValue.Substring(0, pathValue.Length - 10) + "GameUserSettings.ini";
                 pathDisplayBox.Text = pathValue;
                 pathSet = true;
+
+                File.SetAttributes(pathValue, File.GetAttributes(pathValue) & ~FileAttributes.ReadOnly); // Get Attributes, set read only off on Engine.ini
+                File.SetAttributes(gamePathValue, File.GetAttributes(gamePathValue) & ~FileAttributes.ReadOnly); // Get Attributes, set read only off on GameUserSettings.ini
             }
             else
                 pathDisplayBox.Text = "Wrong File Selected (Are you sure you picked Engine.ini?)";
+        }
+
+        static void lineChanger(string newText, string fileName, int line_to_edit)
+        {
+            string[] arrLine = File.ReadAllLines(fileName);
+            if (arrLine[line_to_edit - 1].ToLower().Contains("vsync"))
+                 arrLine[line_to_edit - 1] = newText;
+            File.WriteAllLines(fileName, arrLine);
         }
 
         private Boolean containsString(string @str)
@@ -67,20 +84,24 @@ namespace DeadByDaylight144hz
         {
             if (pathSet)
             {
-                if (!containsString(@"[/script/engine.engine]"))
-                {
-                    writeLine("");
-                    writeLine("");
-                    writeLine(@"[/script/engine.engine]");
-                }
-                if (!containsString(@"bSmoothFrameRate=false")) 
-                    writeLine(@"bSmoothFrameRate=false");       
-                if (!containsString(@"MinSmoothedFrameRate=5"))                
-                    writeLine(@"MinSmoothedFrameRate=5");
-                if (!containsString(@"MaxSmoothedFrameRate=144"))               
-                    writeLine(@"MaxSmoothedFrameRate=144");                
-                if (!containsString(@"bUseVSync=false"))
-                    writeLine(@"bUseVSync=false");
+                lineChanger("bUseVSync=false", gamePathValue, 30); // Changes Vsync setting in Game User Settings
+
+                var first8Lines = File.ReadLines(pathValue).Take(8).ToList(); // Grabs first 8 lines (require lines) in engine.ini, saves
+                File.WriteAllText(pathValue, String.Empty); // Clears text file
+
+                foreach(string curLine in first8Lines) // loops through and prints the require first 8 lines.    
+                    writeLine(curLine);
+
+                writeLine(""); // Blank Space
+               
+                writeLine(@"[/script/engine.engine]"); // Necessary script for changing hz.
+                writeLine(@"bSmoothFrameRate=false");
+                writeLine(@"SmoothedFrameRateRange=(LowerBound=(Type=Inclusive,Value=5.000000),UpperBound=(Type=Exclusive,Value=144.000000))");
+                writeLine(@"bUseVSync=false");
+
+                File.SetAttributes(pathValue, File.GetAttributes(pathValue) | FileAttributes.ReadOnly); // set Engine.ini to read-only again
+                File.SetAttributes(gamePathValue, File.GetAttributes(gamePathValue) | FileAttributes.ReadOnly); // set GameUserSettings.ini to read-only again
+
                 System.Windows.Forms.MessageBox.Show("VSync has been disabled and 144hz has been applied.");
             }
             else
